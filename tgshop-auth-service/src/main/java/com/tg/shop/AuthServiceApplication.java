fe496ac1b1dc23a595c3928e9697e21aa98d1564
lvzhyt <lvzhyt@163.com>
2019-06-19 10:07:29 +0800

    tgshop-auth-service

--- tgshop-auth-service/src/main/java/com/tg/shop/AuthServiceApplication.java
+++ tgshop-auth-service/src/main/java/com/tg/shop/AuthServiceApplication.java
@@ -1,19 +1,13 @@
-package com.tg.shop.auth;
+package com.tg.shop;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author: tg
  * @Date: 2019/3/7 17:39
  */
 @SpringBootApplication
-@EnableDubbo
-@ComponentScan({"com.tg.shop.auth","com.tg.shop.core"})
-@MapperScan("com.tg.shop.auth.mapper")
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class);
